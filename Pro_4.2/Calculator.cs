4fdc23770a009110c837af80f4d23c992900c727
zxcayumi <zxcayumi@qq.com>
2020-12-03 19:50:02 +0800

    增加接口部分

---
+++ Pro_4.2/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pro4_2
+{
+    public class Calculator : ICalculator
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Div(int a, int b)
+        {
+            if (b == 0) throw new DivideByZeroException();
+            return a / b;
+        }
+
+        public int Mul(int a, int b)
+        {
+            return a * b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
