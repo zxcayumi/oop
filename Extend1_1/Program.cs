bafac5ee1b4cd6a0e9907213997df97f3ac8c8a5
zxcayumi <zxcayumi@qq.com>
2021-01-08 23:15:35 +0800

    增加C#9新语法-增强的模式匹配

---
+++ Extend1_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Extend1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "abc";
+            //name = Console.ReadLine();
+
+            //传统写法：null 或着 长度为0
+            //if (name == null || name.Length == 0)
+            //{
+            //    Console.WriteLine("姓名为null或者长度为0");
+            //}
+
+            //新语法写法
+            if (name is null or { Length: 0 })
+            {
+                Console.WriteLine("name为null或者长度为0！");
+            }
+
+            //if (name != null) { }
+
+            if (name is not null)
+            {
+                Console.WriteLine("name不为null");
+            }
+
+            //传统写法
+            //if (name != null && name.Length > 0)
+            //{
+            //    Console.WriteLine("name不为null 并且 长度大于0！");
+            //}
+
+            //新语法写法
+            if (name is not null and { Length: > 0 })
+            {
+                Console.WriteLine("name不为null 并且 长度大于0！");
+            }
+        }
+    }
+}
---
+++ Extend1_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Extend1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //switch的增强模式匹配
+            int age = 0;
+            Console.Write("请输入年龄：");
+            age = int.Parse(Console.ReadLine());
+
+            //传统的：switch只能进行等值比较
+            //新语法：switch可以进行范围的比较
+            /*switch在进行范围比较时，只是一种语法糖（底层本质是转换成了if...else if...else方式）
+              所以这种比较方式，效率和if方式是一样。
+             */
+            switch (age)
+            {
+                case >= 0 and < 3:
+                    Console.WriteLine("bady");
+                    break;
+                case >= 3 and < 14:
+                    Console.WriteLine("child");
+                    break;
+                case >= 14 and < 22:
+                    Console.WriteLine("youth");
+                    break;
+                case >= 22 and < 60:
+                    Console.WriteLine("adult");
+                    break;
+                case >= 60:
+                    Console.WriteLine("old");
+                    break;
+            }
+        }
+    }
+}
